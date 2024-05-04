cf9b0257a4e00a14903ad28e194618ad6dde9c55
Daniel Frank <danjfrank08@gmail.com>
2024-05-03 23:38:06 -0500

    initial push
    PBI1 - Update website (new website)

---
+++ routes/media.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET media page. */
+router.get('/', function(req, res, next) {
+    res.render('media', { title: 'Paris Play | Media'});
+});
+
+module.exports = router;
